64fd1094f0d9a1b87825e521c7991498c191bb8e
TMHolen <TorMx99@gmail.com>
2015-03-19 22:06:32 +0100

    Added links and changed highscore display
    Added links between score and index pages.
    Changed the way highscore list is displayed it's now a table.

--- project/public_html/scores.php
+++ project/public_html/scores.php
@@ -29,40 +29,51 @@
                 <div class="container">><img src="graphicElements/Logo.png" alt="Team4 Logo" id="logo"></div>
             </header>
             <main>
-                <div class="divInlineBlockContaine">
-                    <ul>
-                        <?php
-                        require "dbconnect.php";
+                <div class="center">
+                    <h1>HighScores</h1>
+                    <div class="divInlineBlockContainer autoHeight">
+                        <table>
+                            <tr>
+                                <th>Player:</th>
+                                <th>  Score:  </th>
+                                <th>Date:</th>
+                            </tr>
+                            <?php
+                            require "dbconnect.php";
 
-                        function html_escape($str)
-                        {
-                            return htmlspecialchars($str, ENT_QUOTES);
-                        }
+                            function html_escape($str) {
+                                return htmlspecialchars($str, ENT_QUOTES);
+                            }
 
-                        date_default_timezone_set('Europe/Oslo');
+                            date_default_timezone_set('Europe/Oslo');
 
-                        /* Execute a prepared statement by passing an array of values */
-                        $sth = $dbh->prepare('SELECT name, score, date
+                            /* Execute a prepared statement by passing an array of values */
+                            $sth = $dbh->prepare('SELECT name, score, date
                             FROM highscores
                             ORDER BY score DESC LIMIT 50');
 
-                        $sth->execute();
-                        $scores = $sth->fetchAll();
+                            $sth->execute();
+                            $scores = $sth->fetchAll();
 
 
-                        foreach ($scores as $score) {
-                            ?>
-                            <li>
-                                <?php
-                                echo html_escape($score["name"]), " - ", $score["score"], " - ", date('Y-m-d H:i:s',strtotime($score['date'] . ' UTC'));
+                            foreach ($scores as $score) {
                                 ?>
-                            </li>
-                            <?php
-                        }
-                        ?>
-                    </ul>
+                                <tr>
+                                    <td><?php echo html_escape($score["name"]); ?></td>
+                                    <td><?php echo html_escape($score["score"]); ?></td>
+                                    <td><?php echo html_escape(date('Y-m-d H:i:s', strtotime($score['date'] . ' UTC'))); ?></td>
+
+                                </tr>
+                                <?php
+                            }
+                            ?>
+                        </table>
+
+
+                    </div>
                 </div>
             </main>
+            <a href="index.html"><h2 class="center">Return to game</h2></a>
         </div>
 
         <aside id="Right">
